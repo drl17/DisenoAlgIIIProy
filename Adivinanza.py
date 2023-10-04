0793833562aeba48299dc2436f24a9522a5e9c9b
drl17 <146902806+drl17@users.noreply.github.com>
2023-10-03 20:59:39 -0500

    Add files via upload

---
+++ Adivinanza.py
@@ -0,0 +1,50 @@
+#Esta es la versión final del aplicativo
+
+#Importamos las libreriar para el diseño de aplicativos en web, para invocar datos aleatorios y las ventanas con mensajes
+import tkinter
+import random
+from tkinter import messagebox
+
+#Creamos la interfaz con sus respectivas dimensiones
+Interfaz=tkinter.Tk("Proyecto_DiseñoAlgoritmosIII_Acertijo")
+Interfaz.geometry("480x374")
+
+#Agregamos una imagen de fondo para la interfaz
+Fondo_Interfaz=tkinter.PhotoImage(file="acertijo.png")
+Fondo_Imagen=tkinter.Label(Interfaz,image=Fondo_Interfaz)
+Fondo_Imagen.pack()
+
+#Obtenemos la primera variable como el dato random que elige la máquina
+ganador = random.randint(1, 10)
+
+#Agregamos un texto de bienvenida
+Texto_Bienvenida=tkinter.Label(Interfaz, text="¡Hola, bienvenidos al juego del acertijo!")
+Texto_Bienvenida.place(x=130, y=100)
+
+#Agregamos un texto explicando el juego
+Instruccion1=tkinter.Label(Interfaz, text="Ingresa un número [1 al 10] y comprueba si adivinas el número secreto")
+Instruccion1.place(x=50, y=150)
+
+#Creamos un cuadro de texto para que la interacción del usuario y donde ingresará el número que buscará adivinar
+Caja_Texto=tkinter.Entry(Interfaz)
+Caja_Texto.place(x=170, y=200)
+
+#Definimos la función principal donde comparamos el número obtenido por la máquina y el número ingresado por el usuario
+#Según los escenarios definimos unos resultados
+def comprobar():
+    numero_random=ganador
+    a=int(Caja_Texto.get())
+    if numero_random == a:
+        messagebox.showinfo("Ganaste","Derrotaste al acertijo")
+    else:
+        messagebox.showwarning("Atención", "Sigue intentando")
+
+#Creamos un botón para ejecutar la función principal y la asociamos
+Boton_Comprobar=tkinter.Button(Interfaz, text="Comprobar", command=comprobar)
+Boton_Comprobar.place(x=200, y=250)
+
+#Creamos un botón para cerrar cuando haya finalizado el juego
+Boton_Cerrar=tkinter.Button(Interfaz, text="Cerrar", command=Interfaz.destroy)
+Boton_Cerrar.place(x=215, y=300)
+
+Interfaz.mainloop()
---
+++ Adivinanza2.py
@@ -0,0 +1,89 @@
+#Este es el archivo ecundario donde realizaba todas las pruebas antes de pasar a la versión final
+
+import tkinter
+import random
+from tkinter import messagebox
+
+Interfaz=tkinter.Tk()
+Interfaz.geometry("960x747")
+
+Fondo_Interfaz=tkinter.PhotoImage(file="acertijo.png")
+Fondo_Imagen=tkinter.Label(Interfaz,image=Fondo_Interfaz)
+Fondo_Imagen.pack()
+
+ganador = random.randint(1, 10)
+
+def Obtener_random():
+    numero_random=ganador
+    #print(numero_random)
+
+Boton_ganador=tkinter.Button(Interfaz, text="Obtener random", command=Obtener_random)
+Boton_ganador.place(x=100,y=200)
+
+Texto_Bienvenida=tkinter.Label(Interfaz, text="¡Hola, bienvenidos al juego del acertijo!")
+Texto_Bienvenida.place(x=100, y=50)
+
+Instruccion1=tkinter.Label(Interfaz, text="Ingresa un número [1 al 10] y comprueba si adivinas el número secreto.")
+Instruccion1.place(x=100, y=100)
+
+Caja_Texto=tkinter.Entry(Interfaz)
+Caja_Texto.place(x=100, y=150)
+
+def entero():
+    a=int(Caja_Texto.get())
+    print(a)
+
+Boton_Obtener_CajaTexto=tkinter.Button(Interfaz, text="Obtener de caja texto", command=entero)
+Boton_Obtener_CajaTexto.place(x=100,y=250)
+
+numero=entero
+
+"""def Ventana_Ganador():
+    Ventana=tkinter.Toplevel()
+    Ventana.geometry("300x300")
+    Mensaje_Ganador=tkinter.Label(Ventana_Ganador, text="Adivinaste. Derrotaste al acertijo")
+    Mensaje_Ganador.pack()
+    Boton_CerrarVentanaGanador=tkinter.Button(Ventana_Ganador, text="Cerrar", command=Ventana_Ganador.destroy)
+    Boton_CerrarVentanaGanador.pack()"""
+
+
+def comprobar():
+    numero_random=ganador
+    a=int(Caja_Texto.get())
+    if numero_random == a:
+        #print(f"Adivinaste. El número secreto ganador era: {ganador}")
+        messagebox.showinfo("Ganaste","Derrotaste al acertijo")
+    else:
+        #print("vuelve a intentarlo")
+        messagebox.showwarning("Atención", "Sigue intentando")
+
+Boton_Comprobar=tkinter.Button(Interfaz, text="Comprobar", command=comprobar)
+Boton_Comprobar.place(x=100, y=300)
+
+Boton_Cerrar=tkinter.Button(Interfaz, text="Cerrar", command=Interfaz.destroy)
+Boton_Cerrar.place(x=100, y=350)
+
+Interfaz.mainloop()
+
+#De acuerdo con el último digito de su documento de identidad le quedara asignado el ejercicio correspondiente
+#Juego de Adivinanza: Crea un juego donde el programa selecciona un número aleatorio y el usuario tiene que adivinarlo.
+
+"""import random
+
+print(f"Ingresa números hasta que logres adivinar el número secreto. (Rango 0 - 5)")
+print()
+
+numero = int(input("Ingresa tu número: "))
+
+ganador = random.randint(1, 10)
+
+while True:
+    if numero == ganador:
+        print(f"Adivinaste. El número ganador era: {ganador}")
+        break
+    elif numero < ganador:
+       print(f"Demasiado bajo.")
+       numero = int(input("Ingresa tu número: "))
+    elif numero > ganador:
+        print(f"Demasiado alto.")
+        numero = int(input("Ingresa tu número: "))"""
